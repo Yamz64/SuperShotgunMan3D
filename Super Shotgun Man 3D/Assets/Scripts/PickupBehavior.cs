bad1980795f71dd2eafacdfcce88097b8a3126ee
James Lillicrap <jamesplillicrap@gmail.com>
2022-12-14 23:14:34 -0500

    +Additions
    +Added sprite swap of default enemy with new mobster sprite
    +Added basis for pickups
    +Added small hp, large hp, and """armor""" pickups

--- Super Shotgun Man 3D/Assets/Scripts/PickupBehavior.cs
+++ Super Shotgun Man 3D/Assets/Scripts/PickupBehavior.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class PickupBehavior : MonoBehaviour
 {
+    public int small_hp_bonus, big_hp_bonus, armor_bonus;
     private enum PowerupType { SMALL_HP, BIG_HP, ARMOR, E_ROUNDS, INVINCIBILITY};
     [SerializeField]
     private PowerupType type;
@@ -19,6 +20,43 @@ public class PickupBehavior : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        //if the player is detected, do something to the player based on the powerup
+        if (other.tag != "Player")
+            return;
+
+        bool used = false;
+        PlayerStats stats = other.GetComponent<PlayerStats>();
+
+        switch (type){
+            case PowerupType.SMALL_HP:
+                stats.HP += small_hp_bonus;
+                used = true;
+                break;
+            case PowerupType.BIG_HP:
+                if (stats.HP + big_hp_bonus < 100)
+                {
+                    stats.HP += big_hp_bonus;
+                    used = true;
+                }
+                else if (stats.HP < 100)
+                {
+                    stats.HP = 100;
+                    used = true;
+                }
+                break;
+            case PowerupType.ARMOR:
+                if (stats.AP < 100)
+                {
+                    stats.AP += armor_bonus;
+                    used = true;
+                }
+                break;
+            case PowerupType.E_ROUNDS:
+                break;
+            case PowerupType.INVINCIBILITY:
+                break;
+        }
+        if(used)
+            Destroy(gameObject);
     }
 }
