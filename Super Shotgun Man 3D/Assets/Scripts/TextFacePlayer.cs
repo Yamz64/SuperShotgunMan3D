5233ba7cff2ca405ae39eed016fd093cbf85af19
James Lillicrap <jamesplillicrap@gmail.com>
2022-12-16 23:01:02 -0500

    +Level 1 Additions
    +More level progress
    +Added tutorial rat functionality

--- Super Shotgun Man 3D/Assets/Scripts/TextFacePlayer.cs
+++ Super Shotgun Man 3D/Assets/Scripts/TextFacePlayer.cs
@@ -2,17 +2,33 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+using TMPro;
+
 public class TextFacePlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+    public float fade_distance_min, fade_distance_max;
+
+    private TextMeshPro text;
+
+    void LerpColor()
     {
-        
+        Vector3 player_pos = GameObject.FindGameObjectWithTag("Player").transform.position;
+        float distance = (player_pos - transform.position).magnitude;
+
+        float lerp = (distance - fade_distance_min) / (fade_distance_max - fade_distance_min);
+
+        text.color = Color.Lerp(Color.white, new Color(1.0f, 1.0f, 1.0f, 0.0f), lerp);
     }
 
+    private void Start()
+    {
+        text = GetComponent<TextMeshPro>();
+    }
+    
     // Update is called once per frame
     void Update()
     {
-        
+        LerpColor();
+        transform.forward = -(GameObject.FindGameObjectWithTag("Player").transform.position - transform.position).normalized;
     }
 }
