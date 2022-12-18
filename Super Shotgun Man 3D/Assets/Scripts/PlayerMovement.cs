6ade863a84009ee380931b844be171aaa6334475
James Lillicrap <jamesplillicrap@gmail.com>
2022-12-18 01:25:40 -0500

    +Made it so that only a double shot can make Tony angry

--- Super Shotgun Man 3D/Assets/Scripts/PlayerMovement.cs
+++ Super Shotgun Man 3D/Assets/Scripts/PlayerMovement.cs
@@ -329,10 +329,7 @@ public class PlayerMovement : MonoBehaviour
         if(stats.Shells > 0 && !reloading)
         {
             if (Input.GetButton("Fire1") && !fired)
-            {
-                stats.face.OneTimeAnimationDriver(6);
                 StartCoroutine(FireSequence());
-            }
 
             if (Input.GetButton("Fire2") && !fired && stats.Shells > 1)
             {
@@ -341,10 +338,7 @@ public class PlayerMovement : MonoBehaviour
             }
 
             if (Input.GetButton("Fire2") && !fired && stats.Shells < 2)
-            {
-                stats.face.OneTimeAnimationDriver(6);
                 StartCoroutine(FireSequence());
-            }
 
             if (Input.GetKeyDown(KeyCode.R) && !reloading && stats.Shells < 2)
                 StartCoroutine(ReloadSequence());
