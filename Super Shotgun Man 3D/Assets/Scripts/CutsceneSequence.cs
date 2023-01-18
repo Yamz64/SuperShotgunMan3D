8bb497350f3c22d5453e5c3a9ed42eeebf8fb9bd
James Lillicrap <jamesplillicrap@gmail.com>
2023-01-17 21:23:55 -0500

    +Added skippable cutscenes

--- Super Shotgun Man 3D/Assets/Scripts/CutsceneSequence.cs
+++ Super Shotgun Man 3D/Assets/Scripts/CutsceneSequence.cs
@@ -7,16 +7,19 @@ using UnityEngine.SceneManagement;
 
 public class CutsceneSequence : MonoBehaviour
 {
+    public float skip_text_stay_duration, skip_text_fade_duration;
     public string transition_scene;
-    private bool faded;
+    private float m_skip_text_fade_duration;
+    private bool faded, skip = false;
     private VideoPlayer player;
     private RawImage image;
+    private Text skip_text;
 
     IEnumerator LateStart()
     {
         yield return new WaitForEndOfFrame();
-        yield return new WaitUntil(() => player.isPlaying);
-        yield return new WaitUntil(() => !player.isPlaying);
+        yield return new WaitUntil(() => player.isPlaying || skip);
+        yield return new WaitUntil(() => !player.isPlaying || skip);
         StartCoroutine(Fade());
         yield return new WaitForEndOfFrame();
         yield return new WaitUntil(() => faded);
@@ -39,8 +42,37 @@ public class CutsceneSequence : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        m_skip_text_fade_duration = skip_text_fade_duration;
         player = GetComponent<VideoPlayer>();
         image = GetComponent<RawImage>();
+        skip_text = transform.GetChild(0).GetComponent<Text>();
         StartCoroutine(LateStart());
+    }
+
+    private void Update()
+    {
+        if (Input.anyKeyDown)
+        {
+            if (!skip)
+            {
+                skip = true;
+                if (skip_text_fade_duration > 0.0f)
+                {
+                    m_skip_text_fade_duration = 1.0f;
+                    skip_text_fade_duration = 1.0f;
+                }
+            }
+        }
+        if(skip_text_stay_duration > 0.0f && !skip)
+            skip_text_stay_duration -= Time.deltaTime;
+        else
+        {
+            if (skip_text_fade_duration > 0.0f)
+                skip_text_fade_duration -= Time.deltaTime;
+            else
+                skip_text_fade_duration = 0.0f;
+            
+            skip_text.color = Color.Lerp(new Color(1.0f, 1.0f, 1.0f, 0.0f), Color.white, skip_text_fade_duration / m_skip_text_fade_duration);
+        }
     }
 }
