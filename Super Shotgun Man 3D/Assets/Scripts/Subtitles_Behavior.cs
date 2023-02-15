3ca6b08b786e7e30da1a1133a1c54418aab4c384
FryGuy1348 <77214515+FryGuy1348@users.noreply.github.com>
2023-02-15 18:31:43 -0500

    More stuff
    Made a bounce_pad prefab, and added a cylinder to modify for the wind tunnel
    Started doing some work for subtitles.

---
+++ Super Shotgun Man 3D/Assets/Scripts/Subtitles_Behavior.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+public class Subtitles_Behavior : MonoBehaviour
+{
+    TMP_Text sub_text;
+    Image text_background;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
