43e0006210b22e5247f08ac0dc81500cc1fa7c66
FryGuy1348 <77214515+FryGuy1348@users.noreply.github.com>
2023-02-15 17:51:45 -0500

    Bounce Pad now Bounces
    Force may need to be edited based on the level, currently set to 500.
    Bounce_Pad is the pink cube in the TestScene.
    Not sure if it should be activated by jumping on it, or just colliding.

--- Super Shotgun Man 3D/Assets/Scripts/Bounce_Pad.cs
+++ Super Shotgun Man 3D/Assets/Scripts/Bounce_Pad.cs
@@ -5,14 +5,29 @@ using UnityEngine;
 public class Bounce_Pad : MonoBehaviour
 {
     public bool bounced;
+    public float bounceForce;
 
-    private void OnCollisionEnter(Collision collision)
+    private void OnTriggerEnter(Collider collision)
     {
-        
+        if (collision.gameObject.tag.Equals("Player"))
+        {
+            if (!bounced)
+            {
+                collision.gameObject.GetComponent<Rigidbody>().AddForce(Vector3.up * bounceForce);
+                bounced = true;
+                StartCoroutine(resetBounce());
+            }
+        }
+    }
+    private IEnumerator resetBounce()
+    {
+        yield return new WaitForSeconds(0.5f);
+        bounced = false;
     }
     // Start is called before the first frame update
     void Start()
     {
         bounced = false;
+        if (bounceForce == 0) bounceForce = 500;
     }
 }
