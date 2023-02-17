f173899b3350e404774c17e4dbd62f2bdb123e09
FryGuy1348 <77214515+FryGuy1348@users.noreply.github.com>
2023-02-17 17:03:29 -0500

    Started working on wind tunnel behavior
    Script is attached to individual tubes of the tunnel, may end up changing to a list of points.

---
+++ Super Shotgun Man 3D/Assets/Scripts/Stage_Objects/Wind_Tunnel_Point.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wind_Tunnel : MonoBehaviour
+{
+    public float wind_force;                //Force that the tunnel pulls with
+
+    public Vector3 wind_direction;          //Direction this point in the wind tunnel pushes in
+
+    private void OnTriggerStay(Collider other)
+    {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        if (rb == null)
+            return;
+
+        rb.AddForce(wind_direction * wind_force);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
