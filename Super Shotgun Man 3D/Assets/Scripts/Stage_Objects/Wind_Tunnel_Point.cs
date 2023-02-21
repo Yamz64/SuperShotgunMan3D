4ee1380bcbd0d42f67d4bf95ace63143a694cc42
FryGuy1348 <77214515+FryGuy1348@users.noreply.github.com>
2023-02-20 19:15:36 -0500

    More wind tunnel stuff
    - Added basic wind tunnel as prefab.
    - Wind tunnels take the player's speed on entering into account.
    - Takes the speed on enter, then takes multiplies the speed by the tunnel's wind direction on exit.
    - Need to test further, but the faster you enter the tunnel, the faster you exit.

--- Super Shotgun Man 3D/Assets/Scripts/Stage_Objects/Wind_Tunnel_Point.cs
+++ Super Shotgun Man 3D/Assets/Scripts/Stage_Objects/Wind_Tunnel_Point.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
 public class Wind_Tunnel_Point : MonoBehaviour
 {
@@ -8,6 +9,15 @@ public class Wind_Tunnel_Point : MonoBehaviour
 
     public Vector3 wind_direction;          //Direction this point in the wind tunnel pushes in
 
+    public Vector3 player_speed;            //Speed of the player before entering the tunnel
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+           player_speed = other.gameObject.GetComponent<Rigidbody>().velocity;
+        }
+    }
     private void OnTriggerStay(Collider other)
     {
         Rigidbody rb = other.GetComponent<Rigidbody>();
@@ -15,6 +25,16 @@ public class Wind_Tunnel_Point : MonoBehaviour
             return;
 
         rb.AddForce(wind_direction * wind_force);
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject.tag.Equals("Player"))
+        {
+            float max = player_speed.magnitude;//Math.Max(player_speed.x, Math.Max(player_speed.y, player_speed.z));
+            Vector3 temp = wind_direction * max;
+            other.gameObject.GetComponent<Rigidbody>().velocity += temp;
+            player_speed.Set(0,0,0);
+        }
     }
 
     // Start is called before the first frame update
