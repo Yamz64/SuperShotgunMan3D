a4f02bac861b839dc3b7f57b74782e739ef0ff80
FryGuy1348 <77214515+FryGuy1348@users.noreply.github.com>
2023-04-05 19:29:29 -0400

    Update Tractor_Beam.cs
    - Added cycling behavior for tractor beam
    - Added particle system, need to modify particles color/shape
    - Need to turn off player movement in beam (outside of shotgun)

--- Super Shotgun Man 3D/Assets/Scripts/Stage_Objects/Tractor_Beam.cs
+++ Super Shotgun Man 3D/Assets/Scripts/Stage_Objects/Tractor_Beam.cs
@@ -4,22 +4,42 @@ using UnityEngine;
 
 public class Tractor_Beam : MonoBehaviour
 {
-    GameObject emitter;         //Object emitting the tractor beam (point to pull towards)
-    public float magnitude;     //Force to pull player with
+    GameObject emitter;                 //Object emitting the tractor beam (point to pull towards)
+    public float magnitude;             //Force to pull player with
+
+    public bool cycle = false;          //Whether the beam cycles between on and off
+    bool on;                            //Whether the tractor beam is on or off (true = on, false = off)
+    public float cycle_time;            //Time for tractor beam to cycle between on/off
+    float current;                      //Current time in the cycle
 
     // Start is called before the first frame update
     void Start()
     {
+        on = true;
+        current = cycle_time;
         emitter = this.transform.parent.gameObject;         //Define emitter immediately
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Instantly kill enemies on contact
+        if (other.gameObject.tag.Equals("Enemy"))
+        {
+            other.gameObject.GetComponent<BaseEnemyBehavior>().TakeDamage(other.gameObject.GetComponent<BaseEnemyBehavior>().HP);
+        }
     }
 
     private void OnTriggerStay(Collider other)
     {
-        //Turn off player gravity and pull
-        if (other.gameObject.tag.Equals("Player"))
+        //If the tractor beam is active
+        if (on)
         {
-            other.gameObject.GetComponent<Rigidbody>().useGravity = false;
-            other.transform.position += (emitter.transform.position - other.transform.position).normalized * (Time.deltaTime * magnitude);
+            //Turn off player gravity and pull
+            if (other.gameObject.tag.Equals("Player"))
+            {
+                other.gameObject.GetComponent<Rigidbody>().useGravity = false;
+                other.transform.position += (emitter.transform.position - other.transform.position).normalized * (Time.deltaTime * magnitude);
+            }
         }
     }
 
@@ -29,6 +49,19 @@ public class Tractor_Beam : MonoBehaviour
         if (other.gameObject.tag.Equals("Player"))
         {
             other.gameObject.GetComponent<Rigidbody>().useGravity = true;
+        }
+    }
+
+    private void Update()
+    {
+        if (cycle)
+        {
+            current -= Time.deltaTime;
+            if (current <= 0)
+            {
+                current = cycle_time;
+                on = !on;
+            }
         }
     }
 }
